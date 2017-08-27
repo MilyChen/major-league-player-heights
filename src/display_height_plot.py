13d02c8ec067c17b02bc5aa21c62f8a813ddbe26
Mily <Mily@10.88.111.41>
2017-08-27 13:50:07 -0400

    Added comments

--- src/display_height_plot.py
+++ src/display_height_plot.py
@@ -10,12 +10,11 @@ import matplotlib.pyplot as plt
 
 #======set up data to plot==============================================================================
 data_nhla = pd.read_csv('C:\Users\Mily.mc-pc\Dropbox\MySportsFeed\NHL Data\MYSPORTSFEEDS-ACTIVE_PLAYERS-NHL-20162017REGULAR.csv')
-# data_nhlc = pd.read_csv('C:\Users\Mily.mc-pc\Dropbox\MySportsFeed\NHL Data\MYSPORTSFEEDS-CUMULATIVE_PLAYER_STATS-NHL-20152016REGULAR.csv')
 data_mlb = pd.read_csv('C:\Users\Mily.mc-pc\Dropbox\MySportsFeed\MLB Data\MYSPORTSFEEDS-CUMULATIVE_PLAYER_STATS-MLB-2016REGULAR.csv')
 data_nba = pd.read_csv('C:\Users\Mily.mc-pc\Dropbox\MySportsFeed\NBA Data\MYSPORTSFEEDS-ACTIVE_PLAYERS-NBA-20162017REGULAR.csv')
 data_nfl = pd.read_csv('C:\Users\Mily.mc-pc\Dropbox\MySportsFeed\NFL Data\MYSPORTSFEEDS-ACTIVE_PLAYERS-NFL-20162017REGULAR.csv')
-data = [data_mlb, data_nfl, data_nba, data_nhla]
 
+data = [data_mlb, data_nfl, data_nba, data_nhla]
 colors = ['pink', 'orange', 'yellow', 'blue']
 sports = ['mlb', 'nfl', 'nba', 'nhl']
 shapes = ["*", "x", "^", "D"]
@@ -26,17 +25,20 @@ plot_y_ax = {}
 plt.figure(0)  #
 
 for i, df in enumerate(data):
-    # print df
+    #convert height column from ft'in'' into cm
     df['height_as_decimals'] = pd.to_numeric(df['#Height'].apply(lambda x:np.NaN if pd.isnull(x) else library.height_ftiins_to_cm(x)))
+    #drop all rows with 1+ NaN column values
     df = pd.DataFrame(df[['#Weight', 'height_as_decimals']]).dropna()
     df.height_as_decimals = pd.to_numeric(df.height_as_decimals)
     plot_x_ax[i] = np.array(df['#Weight'])
     plot_y_ax[i] = np.array(df.height_as_decimals)
     df_fit = np.polyfit(plot_x_ax[i], plot_y_ax[i], 1)
     df_fit_fn = np.poly1d(df_fit)
+    #plot each player's height and weight
     plt.plot(plot_x_ax[i], plot_y_ax[i], shapes[i], color=colors[i], alpha=0.2, label=sports[i])
+    #plot line of best fit
     plt.plot(plot_x_ax[i], df_fit_fn(plot_x_ax[i]), '-k', color=colors[i], alpha=0.7)
-    # print(df['#Weight'])
+   
 
 plt.ylabel("Height (cm)")
 plt.xlabel("Weight (lbs)")
@@ -45,10 +47,11 @@ plt.legend(loc='upper left')
 plt.title("Player Height per Major League Sport")
 
 #second plot separated data in subplots====================================================================
-fig_split = plt.figure(1)  #
+fig_split = plt.figure(1)
 
 
 for i, df in enumerate(data):
+    #create a subplot
     ax = fig_split.add_subplot(2, 2, (i + 1))
     ax.set_xlim(150, 350)
     ax.set_ylim(160, 220)
@@ -58,8 +61,9 @@ for i, df in enumerate(data):
      
 plt.suptitle('Major League Player Heights')
 plt.subplots_adjust(hspace=0.36)
- 
-plt.show()  # display all figures
+
+# display all figures
+plt.show()  
 
 
 
--- src/readme.md
+++ src/readme.md
@@ -2,12 +2,12 @@
 >An Introduction to Data Visualization with Pandas
 
 ## Summary 
->Displays the heights and weights of players in a scatter plot for the sports: NHL, MLB, NFL and NBA. 
+>Displays the heights and weights of players in a scatter plot for the sports: NHL, MLB, NFL and NBA. The code generates two views of the data below: 
 
->View #1: ![alt text](https://github.com/MilyChen/major-league-player-heights-and-weights/blob/master/src/figure_0.png "Figure w/ combined data")
->View #2: ![alt text](https://github.com/MilyChen/major-league-player-heights-and-weights/blob/master/src/figure_1.png "Figure w/ subplots")
+> ![alt text](https://github.com/MilyChen/major-league-player-heights-and-weights/blob/master/src/figure_0.png "Figure w/ combined data")
+> ![alt text](https://github.com/MilyChen/major-league-player-heights-and-weights/blob/master/src/figure_1.png "Figure w/ subplots")
 
-
+>
 
 ## Source Data 
 >https://www.mysportsfeeds.com/
